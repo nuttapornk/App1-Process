65990843a30cf127c08c3145682057e2c06591af
nuttaporn kunsin <to_andata@hotmail.com>
2024-04-09 00:48:18 +0700

    fix: remove weatherforcast,add values, add health

---
+++ src/App1-Process/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App1_Process.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("v1/get")]
+        public IActionResult Get()
+        {
+            return Ok(new List<string>() { "1","2","3"});
+        }
+    }
+}
